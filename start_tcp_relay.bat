ac8465fbab08bc07be89268e076c3e812ac9b97e
tanaka-ryuya <ryuyaicehockey@gmail.com>
2025-02-15 11:20:34 +0900

    いろいろ追加

---
+++ start_tcp_relay.bat
@@ -0,0 +1,11 @@
+@echo off
+setlocal
+
+:: 実行ファイルのディレクトリを取得
+set "SCRIPT_DIR=%~dp0"
+set "EXE_PATH=%SCRIPT_DIR%tcp_relay_server2.exe"
+
+:: PowerShell ウィンドウで `tcp_relay_server2.exe` を実行
+start powershell -NoExit -Command "& '%EXE_PATH%' 127.0.0.1:9999 127.0.0.1:10000 --mode connect-listen --dump"
+
+endlocal
